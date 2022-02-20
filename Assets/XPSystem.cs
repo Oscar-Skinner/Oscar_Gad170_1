f36de9ddfb67718ca5e0227641cbaa5f82f502f5
Oscar-Skinner <1027514@student.sae.edu.au>
2022-02-21 00:12:35 +1100

    Finished?
    Mostly Finished. might be one or 2 things missing but we'll find out :)

--- Assets/XPSystem.cs
+++ Assets/XPSystem.cs
@@ -30,34 +30,59 @@ public class XPSystem : MonoBehaviour
             GainXP(attack);
         }
     }
-
+    
+    public void Interaction(int exp)
+    {
+        //interaction called by button press
+        
+    }    
+    
     public void GainXP(float amount)
     {
         //increase our current xp
         curXP += amount;
         Debug.Log("Gained " + amount + " XP");
         Debug.Log("Total XP is now: " + curXP);
+        InitialStats(curXP);
     }
 
-    public void InitialStats()
+    public void InitialStats(float amount)
     {
         //initial stats, level, xp, reqxp, etc...
+        if (amount >= reqXP)
+        {
+            Debug.Log("Epic! You reached a new level!");
+                LevelUp();
+        }
     }
-
-    public void Interaction(int exp)
-    {
-        //interaction called by button press
-    }    
 
     public void LevelUp()
     {
         //increase our level
-
+        level += 1;
+        //reset current to 0
+        curXP = 0;
+        //increase reqXP
+        reqXP *= 1.2f;
+        Debug.Log("You're now Level " + level + "! Congrats!");
+        IncreaseStats();
     }
-
+    
     public void IncreaseStats()
     {
         //improve our stats
-
-    }
+        //improves the health by 5
+        health += 5.0f;
+        //improves the defence by 1.
+        defence += 1.0f;
+        //improves the speed by 10% of original.
+        speed *= 1.1f;
+        //improves the attack by 2.
+        attack += 2.0f;
+        Debug.Log("Your new stats are:");
+            Debug.Log("Health = " + health + ", Defence = " + defence + ", Speed = " + speed + ", Attack = " + attack + ". Good work champ!");
+}
+    
+    
+            
 }
